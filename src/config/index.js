5a531cc2d65e7f7d2aed8017fb16a6ffa7966ff6
Igor Felipe de Jesus <igor.jesus@b2wdigital.com>
2019-04-16 16:41:10 -0300

    aula final

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 const config = {
-  dbUrl: process.env.DB_URL || 'mongodb://estagiobit:estagiobit123@ds139576.mlab.com:39576/game-db'
+  dbUrl: process.env.DB_URL || 'mongodb://estagiobit:estagiobit123@ds139576.mlab.com:39576/game-db',
+  apiOfFireAndIceUrl: 'https://anapioficeandfire.com/api'
 };
 
 export default config;
--- src/constants/error.js
+++ src/constants/error.js
@@ -3,5 +3,10 @@ export default {
     name: 'CharacterNotFoundError',
     status: 404,
     message: 'No character found with that id'
+  },
+  BadRequest: {
+    name: 'BadRequestError',
+    status: 400,
+    message: 'No character found with that ids'
   }
 };
